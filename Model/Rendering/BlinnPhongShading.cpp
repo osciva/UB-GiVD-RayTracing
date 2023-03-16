433af0ec86221d67adfb755a5e03bc560aa70c35
osciva <81873328+osciva@users.noreply.github.com>
2023-03-16 14:18:47 +0100

    Rendering

--- Model/Rendering/BlinnPhongShading.cpp
+++ Model/Rendering/BlinnPhongShading.cpp
@@ -1,7 +1,7 @@
 #include "BlinnPhongShading.hh"
 
 vec3 BlinnPhongShading::shading(shared_ptr<Scene> scene, HitInfo& info, vec3 lookFrom) {
-    //I = kaIa + kdId cos( ⃗ L , ⃗ N ) + ksIs cos( ⃗ N , ⃗ H)β ⃗
+    //I = kaIa + kdId cos( ⃗ L , ⃗ N ) + ksIs cos( ⃗ N , ⃗ H)β
     vec3 ka = info.mat_ptr->Ka;
     vec3 kd = info.mat_ptr->Kd;
     vec3 ks = info.mat_ptr->Ks;
